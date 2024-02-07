e1087bc9c59bbe6613c186de989d708283e77562
“prateekgore” <gorepratik@icloud.com>
2024-02-07 11:44:07 +0530

    HEALTHIFY_ADMIN_GET_TOTAL_COUNT_OF_USER

--- src/main/java/com/healthify/api/HealthifyApplication.java
+++ src/main/java/com/healthify/api/HealthifyApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-
 public class HealthifyApplication {
 
 	public static void main(String[] args) {
