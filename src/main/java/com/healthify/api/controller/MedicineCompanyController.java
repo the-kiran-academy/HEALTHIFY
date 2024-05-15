975aa3d27eadbc55b57d15818d85959acee1f4ce
soni-jondhale <jondhalesunita753@gmail.com>
2024-05-15 16:38:52 +0530

    HEALTHYFY_Add_Medicine_Companyname

--- src/main/java/com/healthify/api/controller/MedicineCompanyController.java
+++ src/main/java/com/healthify/api/controller/MedicineCompanyController.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import javax.validation.Valid;
@@ -26,7 +26,7 @@ public class MedicineCompanyController {
 	@Autowired
 	private MedicineCompanyService medicineCompanyService;
 
-	@GetMapping("/add")
+	@PostMapping("/add")
 	public ResponseEntity<Object> addMedicineCompany(@RequestBody @Valid MedicineCompany medicineCompany) {
 
 		try {
